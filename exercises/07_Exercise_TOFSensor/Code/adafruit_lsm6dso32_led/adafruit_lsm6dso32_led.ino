9b898bdf6381c7d8bdc981c49b1fad10be0a562e
audrilli <audreymeret.lohmann@zhdk.ch>
2022-10-31 13:48:03 +0100

    Add Ex 7

---
+++ exercises/07_Exercise_TOFSensor/Code/adafruit_lsm6dso32_led/adafruit_lsm6dso32_led.ino
@@ -0,0 +1,125 @@
+// Basic demo for accelerometer & gyro readings from Adafruit
+// LSM6DSO32 sensor
+
+#include <Adafruit_LSM6DSO32.h>
+
+// For SPI mode, we need a CS pin
+#define LSM_CS 10
+// For software-SPI mode we need SCK/MOSI/MISO pins
+#define LSM_SCK 13
+#define LSM_MISO 12
+#define LSM_MOSI 11
+
+//Led code
+#define led 4
+#define brightness ;
+
+Adafruit_LSM6DSO32 dso32;
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);  // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit LSM6DSO32 test!");
+
+  if (!dso32.begin_I2C()) {
+    // if (!dso32.begin_SPI(LSM_CS)) {
+    // if (!dso32.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
+    // Serial.println("Failed to find LSM6DSO32 chip");
+    while (1) {
+      delay(10);
+    }
+    //led pinmode
+    pinMode(led, OUTPUT);
+  }
+
+  Serial.println("LSM6DSO32 Found!");
+
+  dso32.setAccelRange(LSM6DSO32_ACCEL_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (dso32.getAccelRange()) {
+    case LSM6DSO32_ACCEL_RANGE_4_G:
+      Serial.println("+-4G");
+      break;
+    case LSM6DSO32_ACCEL_RANGE_8_G:
+      Serial.println("+-8G");
+      break;
+    case LSM6DSO32_ACCEL_RANGE_16_G:
+      Serial.println("+-16G");
+      break;
+    case LSM6DSO32_ACCEL_RANGE_32_G:
+      Serial.println("+-32G");
+      break;
+  }
+
+
+  // dso32.setAccelDataRate(LSM6DS_RATE_12_5_HZ);
+  Serial.print("Accelerometer data rate set to: ");
+  switch (dso32.getAccelDataRate()) {
+    case LSM6DS_RATE_SHUTDOWN:
+      Serial.println("0 Hz");
+      break;
+    case LSM6DS_RATE_12_5_HZ:
+      Serial.println("12.5 Hz");
+      break;
+    case LSM6DS_RATE_26_HZ:
+      Serial.println("26 Hz");
+      break;
+    case LSM6DS_RATE_52_HZ:
+      Serial.println("52 Hz");
+      break;
+    case LSM6DS_RATE_104_HZ:
+      Serial.println("104 Hz");
+      break;
+    case LSM6DS_RATE_208_HZ:
+      Serial.println("208 Hz");
+      break;
+    case LSM6DS_RATE_416_HZ:
+      Serial.println("416 Hz");
+      break;
+    case LSM6DS_RATE_833_HZ:
+      Serial.println("833 Hz");
+      break;
+    case LSM6DS_RATE_1_66K_HZ:
+      Serial.println("1.66 KHz");
+      break;
+    case LSM6DS_RATE_3_33K_HZ:
+      Serial.println("3.33 KHz");
+      break;
+    case LSM6DS_RATE_6_66K_HZ:
+      Serial.println("6.66 KHz");
+      break;
+  }
+
+  void loop() {
+
+    //  /* Get a new normalized sensor event */
+    sensors_event_t accel;
+
+    dso32.getEvent(&accel);
+
+
+    /* Display the results (acceleration is measured in m/s^2) */
+    Serial.print("\t\tAccel X: ");
+    Serial.print(accel.acceleration.x);
+    Serial.print(" \tY: ");
+    Serial.print(accel.acceleration.y);
+    Serial.print(" \tZ: ");
+    Serial.print(accel.acceleration.z);
+    Serial.println(" m/s^2 ");
+
+    //ledLogic brightness, this code maps the sensor event to the brightness of the led
+    //When the accelometer is too high, the Led turns off
+
+    brightness = map(accel.acceleration.x,18,27,0,255));
+    analogWrite(led, brightness);
+
+    if (brightness = 27) {
+      analogwrite(led, LOW)
+    } else (
+      analogWrite(led, brightness);)
+
+
+
+      delay(100);
+  }
---
+++ exercises/09_Neopixel-sensor/Code/adafruit_lsm6ds33_test_TEMPSENS_neopixel/adafruit_lsm6ds33_test_TEMPSENS_neopixel.ino
@@ -0,0 +1,92 @@
+// Basic demo for accelerometer/gyro readings from Adafruit LSM6DS33
+
+#include <Adafruit_LSM6DS33.h>
+
+// For SPI mode, we need a CS pin
+#define LSM_CS 10
+// For software-SPI mode we need SCK/MOSI/MISO pins
+#define LSM_SCK 13
+#define LSM_MISO 12
+#define LSM_MOSI 11
+
+//NeopixelCode
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
+#endif
+#define PIN 6
+#define NUMPIXELS 8
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500
+
+
+Adafruit_LSM6DS33 lsm6ds33;
+void setup(void) {
+  Serial.begin(115200);
+  while (!Serial)
+    delay(10);  // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("Adafruit LSM6DS33 test!");
+
+  if (!lsm6ds33.begin_I2C()) {
+    // if (!lsm6ds33.begin_SPI(LSM_CS)) {
+    // if (!lsm6ds33.begin_SPI(LSM_CS, LSM_SCK, LSM_MISO, LSM_MOSI)) {
+    Serial.println("Failed to find LSM6DS33 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+
+  Serial.println("LSM6DS33 Found!");
+
+
+  lsm6ds33.configInt1(false, false, true);  // accelerometer DRDY on INT1
+  lsm6ds33.configInt2(false, true, false);  // gyro DRDY on INT2
+
+//Void Setup Neopixel
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+  clock_prescale_set(clock_div_1);
+#endif
+  pixels.begin();
+}
+
+void loop() {
+  //  /* Get a new normalized sensor event */
+  sensors_event_t accel;
+  sensors_event_t gyro;
+  sensors_event_t temp;
+  lsm6ds33.getEvent(&accel, &gyro, &temp);
+
+  Serial.print("\t\tTemperature ");
+  Serial.print(temp.temperature);
+  Serial.println(" deg C");
+
+float color = map(temp.temperature, 24.5, 26, 0, 255);
+
+  delay(100);
+
+  //void loop NEOPIXEL
+  for (int i=0; i<NUMPIXELS; i++){
+pixels.setPixelColor(i, pixels.Color(color , 0, 255-color));
+  }
+  
+  pixels.show();
+
+
+
+  //  // serial plotter friendly format
+
+  //  Serial.print(temp.temperature);
+  //  Serial.print(",");
+
+  //  Serial.print(accel.acceleration.x);
+  //  Serial.print(","); Serial.print(accel.acceleration.y);
+  //  Serial.print(","); Serial.print(accel.acceleration.z);
+  //  Serial.print(",");
+
+  // Serial.print(gyro.gyro.x);
+  // Serial.print(","); Serial.print(gyro.gyro.y);
+  // Serial.print(","); Serial.print(gyro.gyro.z);
+  // Serial.println();
+  //  delayMicroseconds(10000);
+}
